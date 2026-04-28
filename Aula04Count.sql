ec50b7ecc9347846ac4ef709a90dc536d82565ff
EduardoFreitas23 <edu.c.freitas23@outlook.com>
2026-04-27 23:47:30 -0300

    docs(count): contagem de registros com agregacao

---
+++ Aula04Count.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(DISTINCT title)
+FROM Person.Person;
+
+-- Desafio 1
+SELECT COUNT(*)
+FROM Production.Product;
+
+-- Desafio 2
+SELECT COUNT(size)
+FROM Production.Product;
+
+-- Desafio 3
+SELECT COUNT(DISTINCT size)
+FROM Production.Product;
